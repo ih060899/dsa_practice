8ef17cce6520776eefd98d9eb88319770d63972c
Imran Hossain <ih060899@gmail.com>
2023-10-07 16:09:55 +0600

    init arrays

--- src/arrays/Array.java
+++ src/arrays/Array.java
@@ -1,7 +1,28 @@
 package arrays;
 
 public class Array {
-    public Array(int length){
+    private int[] items;
+    private int count;
 
+    public Array(int length) {
+        items = new int[length];
+    }
+
+    public void insert(int item){
+        if(items.length == count){
+            int[] newItems = new int[count * 2];
+//            System.arraycopy(items, 0, newItems, 0, count);
+            for (int i = 0; i < count; i++) {
+                newItems[i] = items[i];
+            }
+            items = newItems;
+        }
+        items[count++] = item;
+    }
+
+    public void print(){
+        for (int i = 0; i < count; i++) {
+            System.out.println(items[i]);
+        }
     }
 }
--- src/arrays/ArraysMain.java
+++ src/arrays/ArraysMain.java
@@ -1,9 +1,13 @@
 package arrays;
 
-import java.util.Arrays;
-
 public class ArraysMain {
     public static void main(String[] args) {
-
+        Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.insert(50);
+        numbers.print();
     }
 }
