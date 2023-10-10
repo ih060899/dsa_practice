f65f26d6569c73c50b177ce324444ddf7085d3bc
ih060899 <ih060899@gmail.com>
2023-10-10 15:37:15 +0600

    hashtables

---
+++ src/hashtables/HashTablesMain.java
@@ -0,0 +1,9 @@
+package hashtables;
+
+public class HashTablesMain {
+    public static void main(String[] args) {
+//        System.out.println(CharFinder.findFirstNonRepeatingChar("a green apple"));
+//        System.out.println(CharFinder.findFirstRepeatedChar("a green apple"));
+
+    }
+}
