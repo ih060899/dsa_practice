08e19084510791419aab22a1a56db6494ba9867f
Imran Hossain <ih060899@gmail.com>
2023-10-11 23:38:07 +0600

    BubbleSort SelectionSort InsertionSort

---
+++ src/sort/InsertionSort.java
@@ -0,0 +1,23 @@
+package sort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] numbers = {7, 3, 1, 4, 6, 2, 3};
+        sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    public static void sort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            var current = array[i];
+            var j = i -1;
+           while (j >= 0 && array[j] > current){
+               array[j+1] = array[j];
+               j--;
+           }
+           array[j + 1] = current;
+        }
+    }
+}
