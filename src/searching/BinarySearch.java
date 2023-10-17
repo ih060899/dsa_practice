3f5bcedd239086fe1367d2079af2403ede975640
ih060899 <ih060899@gmail.com>
2023-10-17 17:12:50 +0600

    search

---
+++ src/searching/BinarySearch.java
@@ -0,0 +1,11 @@
+package searching;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+
+    }
+
+    public static int binarySearch(int[] array, int target){
+        return -1;
+    }
+}
---
+++ src/searching/LinearSearch.java
@@ -0,0 +1,16 @@
+package searching;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] numbers = {7, 1, 3, 6, 5};
+        var index = linearSearch(numbers, 60);
+        System.out.println(index);
+    }
+
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) return i;
+        }
+        return -1;
+    }
+}
