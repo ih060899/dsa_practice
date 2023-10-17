1d1cff361eaa7b042dc591482a15905b06d5a9e3
Imran Hossain <ih060899@gmail.com>
2023-10-17 22:14:16 +0600

    binary search

---
+++ src/searching/TernarySearch.java
@@ -0,0 +1,8 @@
+package searching;
+
+public class TernarySearch {
+    public static int ternarySearch(int[] array, int target, int left, int right){
+        int partitionSize = (right - left) / 3;
+        return -1;
+    }
+}
