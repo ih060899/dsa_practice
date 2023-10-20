f2cf91ea119c051de7e2fed49a8e7476f1f39753
Imran Hossain <ih060899@gmail.com>
2023-10-21 00:55:42 +0600

    string manipulation

--- src/StringManipulation/StringMain.java
+++ src/StringManipulation/StringMain.java
@@ -5,7 +5,19 @@ public class StringMain {
 //        int count = StringUtils.countVowels("HellO World");
 //        System.out.println(count);
 
-        String reversed = StringUtils.reverse("Hello World");
-        System.out.println(reversed);
+//        String reversed = StringUtils.reverse("Hello World");
+//        System.out.println(reversed);
+
+//        String reverseWords = StringUtils.reverseWords("  Trees are beautiful  ");
+//        System.out.println("'"+reverseWords+"'");
+
+//        boolean areRotations = StringUtils.areRotations("ABCD", "ADBC");
+//        System.out.println(areRotations);
+
+//        String removeDuplicates = StringUtils.removeDuplicates("Trees are beautiful");
+//        System.out.println(removeDuplicates);
+
+        char result = StringUtils.getMaxOccuringChar("Traas are beautiful");
+        System.out.println(result);
     }
 }
--- src/StringManipulation/StringUtils.java
+++ src/StringManipulation/StringUtils.java
@@ -1,5 +1,10 @@
 package StringManipulation;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class StringUtils {
     public static int countVowels(String str) {
         if (str == null)
@@ -27,5 +32,78 @@ public class StringUtils {
         return reverse.toString();
     }
 
+    public static String reverseWords(String sentence) {
+        if (sentence == null)
+            return "";
 
+        String[] words = sentence.trim().split(" ");
+        Collections.reverse(Arrays.asList(words));
+        return String.join(" ", words);
+//        StringBuilder reversed = new StringBuilder();
+//        for (int i = words.length - 1; i >= 0; i--) {
+//            reversed.append(words[i] + " ");
+//        }
+//        return reversed.toString().trim();
+    }
+
+    public static boolean areRotations(String str1, String str2) {
+        if (str1 == null && str2 == null)
+            return false;
+        return (str1.length() == str2.length()) &&
+                (str1 + str1).contains(str2);
+
+//        if (str1.length() != str2.length())
+//            return false;
+//
+//        if (!(str1 + str1).contains(str2))
+//           return false;
+//
+//        return true;
+    }
+
+    public static String removeDuplicates(String str) {
+        if (str == null)
+            return "";
+
+        StringBuilder output = new StringBuilder();
+        Set<Character> seen = new HashSet<>();
+        for (var ch : str.toCharArray()) {
+            if (!seen.contains(ch)) {
+                seen.add(ch);
+                output.append(ch);
+            }
+        }
+        return output.toString();
+    }
+
+    public static char getMaxOccuringChar(String str) {
+        if (str == null || str.isEmpty())
+            throw new IllegalArgumentException();
+
+        final int ASCII_SIZE = 256;
+        int[] frequencies = new int[ASCII_SIZE];
+
+        for (var ch : str.toCharArray()) {
+            frequencies[ch]++;
+        }
+
+        var max = 0;
+        char result = ' ';
+        for (int i = 0; i < frequencies.length; i++) {
+            if (frequencies[i] > max) {
+                max = frequencies[i];
+                result = (char) i;
+            }
+        }
+
+        return result;
+
+//        Map<Character, Integer> frequencies = new HashMap<>();
+//        for (var ch : str.toCharArray()){
+//            if (frequencies.containsKey(ch))
+//                frequencies.replace(ch, frequencies.get(ch) + 1);
+//            else
+//                frequencies.put(ch, 1);
+//        }
+    }
 }
