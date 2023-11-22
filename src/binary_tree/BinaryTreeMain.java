2290be6f5c9838f30037050acd56090ada9fd5db
Imran Hossain <ih060899@gmail.com>
2023-11-22 22:28:59 +0600

    binery tree

--- src/binary_tree/BinaryTreeMain.java
+++ src/binary_tree/BinaryTreeMain.java
@@ -1,6 +1,5 @@
 package binary_tree;
 
-import java.io.FilterOutputStream;
 
 public class BinaryTreeMain {
     public static void main(String[] args) {
@@ -12,6 +11,16 @@ public class BinaryTreeMain {
         tree.insert(6);
         tree.insert(8);
         tree.insert(10);
-        System.out.println(tree.find(4));
+//        System.out.println(tree.find(4));
+//        tree.traversePreOrder();
+//        System.out.println();
+//        tree.traverseInOrder();
+//        System.out.println();
+//        tree.traverseInOrderDesc();
+//        System.out.println();
+//        tree.traversePostOrder();
+
+//        System.out.println(tree.height());
+        System.out.println(tree.min());
     }
 }
--- src/binary_tree/Tree.java
+++ src/binary_tree/Tree.java
@@ -40,6 +40,102 @@ public class Tree {
         return false;
     }
 
+    public void traversePreOrder() {
+        traversePreOrder(root);
+    }
+
+    public void traverseInOrder() {
+        traverseInOrder(root);
+    }
+
+    public void traverseInOrderDesc() {
+        traverseInOrderDesc(root);
+    }
+
+    public void traversePostOrder() {
+        traversePostOrder(root);
+    }
+
+    private void traversePreOrder(Node root) {
+        if (root == null)
+            return;
+
+        System.out.print(root.value + " ");
+        traversePreOrder(root.leftChild);
+        traversePreOrder(root.rightChild);
+    }
+
+    private void traverseInOrder(Node root) {
+        if (root == null)
+            return;
+
+        traverseInOrder(root.leftChild);
+        System.out.print(root.value + " ");
+        traverseInOrder(root.rightChild);
+    }
+
+    private void traverseInOrderDesc(Node root) {
+        if (root == null)
+            return;
+
+        traverseInOrderDesc(root.rightChild);
+        System.out.print(root.value + " ");
+        traverseInOrderDesc(root.leftChild);
+    }
+
+
+    private void traversePostOrder(Node root) {
+        if (root == null)
+            return;
+
+        traversePostOrder(root.leftChild);
+        traversePostOrder(root.rightChild);
+        System.out.print(root.value + " ");
+    }
+
+    public int height() {
+        return height(root);
+    }
+
+    private int height(Node root) {
+        if (root == null)
+            return -1;
+
+        if (isLeaf(root))
+            return 0;
+        return 1 + Math.max(height(root.leftChild), height(root.rightChild));
+    }
+
+    private static boolean isLeaf(Node root) {
+        return root.leftChild == null && root.rightChild == null;
+    }
+
+    // O(log n)
+    public int min() {
+        if (root == null)
+            throw new IllegalArgumentException();
+
+        var current = root;
+        var last = current;
+        while (current != null) {
+            last = current;
+            current = current.leftChild;
+        }
+        return last.value;
+//        return min(root);
+    }
+
+    // O(n)
+    private int min(Node root) {
+        if (root == null)
+            return Integer.MAX_VALUE;
+        if (isLeaf(root))
+            return root.value;
+        var left = min(root.leftChild);
+        var right = min(root.rightChild);
+        return Math.min(root.value, Math.min(left, right));
+    }
+
     private class Node {
         private int value;
         private Node leftChild;
