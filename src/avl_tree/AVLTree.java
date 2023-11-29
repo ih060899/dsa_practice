00eb4e175e719ef857b6500687e78efcd9f7d375
ih060899 <ih060899@gmail.com>
2023-11-29 12:50:28 +0600

    AVL Tree

--- src/avl_tree/AVLTree.java
+++ src/avl_tree/AVLTree.java
@@ -1,4 +1,90 @@
 package avl_tree;
 
 public class AVLTree {
+    private AVLNode root;
+
+    public void insert(int value) {
+        root = insert(root, value);
+    }
+
+    public AVLNode insert(AVLNode root, int value) {
+        if (root == null)
+            return new AVLNode(value);
+
+        if (value < root.value)
+            root.leftChild = insert(root.leftChild, value);
+        else
+            root.rightChild = insert(root.rightChild, value);
+
+        setHeight(root);
+        return balance(root);
+
+    }
+
+    private AVLNode balance(AVLNode root) {
+        if (isLeftHeavy(root)) {
+            if (balanceFactor(root.leftChild) < 0)
+                root.leftChild = rotateLeft(root.leftChild);
+            return rotateRight(root);
+        } else if (isRightHeavy(root)) {
+            if (balanceFactor(root.rightChild) > 0)
+                root.rightChild = rotateRight(root.rightChild);
+            return rotateLeft(root);
+        }
+        return root;
+    }
+
+    private AVLNode rotateLeft(AVLNode root) {
+        var newRoot = root.rightChild;
+        root.rightChild = newRoot.leftChild;
+        newRoot.leftChild = root;
+        setHeight(root);
+        setHeight(newRoot);
+        return newRoot;
+    }
+
+    private AVLNode rotateRight(AVLNode root) {
+        var newRoot = root.leftChild;
+        root.leftChild = newRoot.rightChild;
+        newRoot.rightChild = root;
+        setHeight(root);
+        setHeight(newRoot);
+        return newRoot;
+    }
+
+    private void setHeight(AVLNode root) {
+        root.height = Math.max(height(root.leftChild), height(root.rightChild)) + 1;
+    }
+
+    private int balanceFactor(AVLNode node) {
+        return (node == null) ? 0 : height(node.leftChild) - height(node.rightChild);
+    }
+
+    private boolean isLeftHeavy(AVLNode node) {
+        return balanceFactor(node) > 1;
+    }
+
+    private boolean isRightHeavy(AVLNode node) {
+        return balanceFactor(node) < -1;
+    }
+
+    private int height(AVLNode node) {
+        return (node == null) ? -1 : node.height;
+    }
+
+    private class AVLNode {
+        private int height;
+        private int value;
+        private AVLNode leftChild;
+        private AVLNode rightChild;
+
+        public AVLNode(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "value=" + this.value;
+        }
+    }
 }
