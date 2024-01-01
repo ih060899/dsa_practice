68de2a8394998132c16e70b5ed5338d9fa88134b
ih060899 <ih060899@gmail.com>
2024-01-01 12:22:56 +0600

    383. Ransom Note

---
+++ src/problem/RansomNote.java
@@ -0,0 +1,71 @@
+package problem;
+
+//383. Ransom Note
+//Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+//
+//Each letter in magazine can only be used once in ransomNote.
+//
+//
+//
+//        Example 1:
+//
+//Input: ransomNote = "a", magazine = "b"
+//Output: false
+//Example 2:
+//
+//Input: ransomNote = "aa", magazine = "ab"
+//Output: false
+//Example 3:
+//
+//Input: ransomNote = "aa", magazine = "aab"
+//Output: true
+//
+//
+//Constraints:
+//
+//        1 <= ransomNote.length, magazine.length <= 105
+//ransomNote and magazine consist of lowercase English letters.
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RansomNote {
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
+
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
+
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
+        }
+        return true;
+    }
+
+    public static boolean canConstruct1(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch: magazine.toCharArray()){
+            if (map.containsKey(ch)){
+                map.put(ch, map.get(ch) + 1);
+            }else {
+                map.put(ch, 1);
+            }
+        }
+        for (char ch: ransomNote.toCharArray()){
+            if (!map.containsKey(ch) || map.get(ch) <= 0){
+                return false;
+            }
+            map.put(ch, map.get(ch) - 1);
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(canConstruct1("a", "b"));
+        System.out.println(canConstruct1("aa", "ab"));
+        System.out.println(canConstruct1("aa", "aab"));
+        System.out.println(canConstruct1("aab", "kdjfdjkl  aarejiaoeoiazsas  fdfed"));
+    }
+}
