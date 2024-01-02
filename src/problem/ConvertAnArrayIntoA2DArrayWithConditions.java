153b3ac6ce6d5af8228f2ca72dd8c1a8683a9cf2
ih060899 <ih060899@gmail.com>
2024-01-02 11:43:07 +0600

    2610. Convert an Array Into a 2D Array With Conditions

--- src/problem/ConvertAnArrayIntoA2DArrayWithConditions.java
+++ src/problem/ConvertAnArrayIntoA2DArrayWithConditions.java
@@ -39,8 +39,8 @@ import java.util.*;
 
 public class ConvertAnArrayIntoA2DArrayWithConditions {
     public static void main(String[] args) {
-        System.out.println(findMatrix1(new int[]{1,3,4,1,2,3,1}));
-        System.out.println(findMatrix1(new int[]{1,2,3,4}));
+        System.out.println(findMatrix2(new int[]{1,3,4,1,2,3,1}));
+        System.out.println(findMatrix2(new int[]{1,2,3,4}));
     }
     public static List<List<Integer>> findMatrix(int[] nums) {
         Map<Integer, Integer> um = new HashMap<>();
@@ -93,5 +93,32 @@ public class ConvertAnArrayIntoA2DArrayWithConditions {
         }
 
         return result;
+    }
+
+    public static List<List<Integer>> findMatrix2(int[] nums) {
+
+        Map<Integer, Integer> numCount = new HashMap<>();
+        List<List<Integer>> nestedArray = new ArrayList<>();
+
+        for (int num : nums) {
+            if (numCount.containsKey(num)) numCount.put(num, numCount.get(num) + 1);
+            else numCount.put(num, 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : numCount.entrySet()) {
+            Integer num = entry.getKey();
+
+            for (int i = 0; i < entry.getValue(); i++) {
+                if (nestedArray.size()- 1 >= i ) {
+                    List<Integer> secondArray = nestedArray.get(i);
+                    secondArray.add(num);
+                } else {
+                    nestedArray.add(i, new ArrayList<>(List.of(num)));
+                }
+
+            }
+
+        }
+        return nestedArray;
     }
 }
